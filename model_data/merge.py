d3cb744818c32cdae2cfca30ec9567b4041ab86b
victor <osiajod@gmail.com>
2019-11-19 23:39:26 -0500

    street light zipcode conversion and +'0' added to fix str

--- model_data/merge.py
+++ model_data/merge.py
@@ -12,27 +12,34 @@ def to_zipcode(lat_long_series):
     long = lat_long_series[1]
     return search.by_coordinates(lat,long,radius=30)[0].values()[0]
 
-crime_zip = pd.DataFrame(crime[["Lat","Long"]].head().apply(to_zipcode, axis=1), columns=["zipcode"])
+crime_zip = pd.DataFrame(crime[["Lat","Long"]].apply(to_zipcode, axis=1), columns=["zipcode"])
 
 crime_zip.head()
 
 crime_with_zip = pd.concat([crime, crime_zip], axis=1)
 crime_with_zip.to_csv("crime_with_zip.csv", index=False)
+
 print("finished converting crime")
+
 #now do the same with Boston population
 boston = pd.read_csv('./Boston_population_density.csv')
+boston['Zipcode']='0'+boston['Zipcode'].astype(str)
 boston.rename(columns = {'Zipcode':'zipcode'}, inplace = True)
 
+
 bldg = pd.read_csv('./bldg_zip.csv')
+bldg['Zipcode']='0'+bldg['Zipcode'].astype(str)
 bldg.rename(columns = {'ZIPCODE' : 'zipcode'}, inplace = True)
 
 land = pd.read_csv('./land_zip.csv')
+land['Zipcode']='0'+land['Zipcode'].astype(str)
 land.rename(columns = {'ZIPCODE' : 'zipcode'}, inplace = True)
 
 light = pd.read_csv('./streetlight_locations.csv')
 light_zip = pd.DataFrame(light[["Lat","Long"]].apply(to_zipcode, axis=1), columns=["zipcode"])
 light_with_zip = pd.concat([light, light_zip], axis=1)
 light_with_zip.to_csv("light_with_zip.csv", index=False)
+
 print("finished converting light")
 
 crime_with_zip_joined = crime_with_zip.join(boston.set_index('zipcode'), on='zipcode')
